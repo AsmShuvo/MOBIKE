5c99ae6277d3261e297f7d596bdba677d4ece28f
AsmShuvo <hoffmanhulk99@gmail.com>
2024-07-07 01:41:10 +0600

    add:payment method to db, del: paid items from cart

--- src/Pages/CheckoutForm/CheckoutForm.jsx
+++ src/Pages/CheckoutForm/CheckoutForm.jsx
@@ -1,9 +1,40 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { loadStripe } from '@stripe/stripe-js';
+import { useContext, useEffect, useState } from "react";
+import useAxios from "../../hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+import { AuthContext } from "../../Providers/AuthProviders";
 
 const CheckoutForm = () => {
+    const { user } = useContext(AuthContext)
+    const [error, setError] = useState('');
+    const [trx, setTrx] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+
     const stripe = useStripe();
     const elements = useElements();
+    const axios = useAxios();
+    const { data: cart, refetch: cartRefetch } = useQuery({
+        queryKey: ["cart_"],
+        queryFn: async () => {
+            const res = await axios.get("/cart");
+            return res.data;
+        }
+    });
+    console.log(cart);
+    const price = cart?.reduce((total, item) => total + item.rent, 0)
+    console.log(price);
+
+    useEffect(() => {
+        axios.post('/create-payment-intent', { price })
+            .then(res => {
+                console.log(res.data.clientSecret);
+                setClientSecret(res.data.clientSecret);
+            })
+            .catch(err => {
+                console.log("Error while posting", err)
+            })
+    }, [axios, price])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -19,12 +50,43 @@ const CheckoutForm = () => {
         })
 
         if (error) {
-            console.log("payment error")
+            console.log("payment error");
+            setError(error.message)
         }
         else {
-            console.log("payment method", paymentMethod)
+            console.log("payment method", paymentMethod);
+            setError("")
         }
+        // confitm payment
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    email: user?.email || "anonymus",
+                    name: user?.displayName || "anonymus"
+                }
+            }
+        })
 
+        if (confirmError) {
+            console.log("confirm error", confirmError)
+        }
+        else {
+            console.log("payment intent", paymentIntent);
+            console.log("trx id", paymentIntent.id);
+            setTrx(paymentIntent.id)
+            const payment = {
+                email: user?.email,
+                price,
+                date: new Date(),
+                cartIds: cart?.map(item => item._id),
+                status: 'pending',
+                transactionID: paymentIntent.id
+            }
+            console.log("cart ids", payment.cartIds);
+            const res = axios.post('/payments', payment)
+            console.log("payment saved", res.data)
+        }
 
     }
     return (
@@ -50,8 +112,11 @@ const CheckoutForm = () => {
                     }}
                 />
                 <div className="flex justify-center items-center" >
-                    <button type="submit" className="mx-auto bg-blue-700 hover:bg-success text-white font-semibold btn btn-sm" disabled={!stripe}>Pay</button>
+                    <button type="submit" disabled={!stripe || !clientSecret} className="mx-auto bg-blue-700 hover:bg-success text-white font-semibold btn btn-sm"  >Pay</button>
+                    <p className="text-red-600">{error}</p>
+                    {trx && <p className="text-blue-600"> TRX ID {trx}</p>}
                 </div>
+
             </form>
         </div>
     );
